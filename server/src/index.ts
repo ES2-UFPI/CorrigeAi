976cf27cb435f3d82f2aac36e5a1b627760f3dca
fullpistoladev <mateus_henrique2011@outlook.com>
2023-01-27 15:33:51 -0300

    adding conection to database using mongoose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 const app = express();
+
+const db = await mongoose.connect('mongodb+srv://corrigeai:<corrigeai123>@corrigeai.cdc4okk.mongodb.net/?retryWrites=true&w=majority');
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
