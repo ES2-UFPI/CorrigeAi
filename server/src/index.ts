d87b985042bbea08549560220211c5be19ce5025
fullpistoladev <mateus_henrique2011@outlook.com>
2023-01-30 21:09:36 -0300

    using .env files to protect db passwords #12

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
+import { config } from 'dotenv'
+config();
+
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
-const app = express();
 
-const db = await mongoose.connect('mongodb+srv://corrigeai:<corrigeai123>@corrigeai.cdc4okk.mongodb.net/?retryWrites=true&w=majority');
+const app = express();
+mongoose.set('strictQuery', false);
+const PORT = 3000;
+
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 }); 
 
-app.listen(5000); 
+app.listen(4000); 
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT);
+});
 
