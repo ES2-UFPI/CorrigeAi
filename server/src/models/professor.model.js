3ea53d3ce6169c9e9a7e385dd0f608f9182c350b
Lucas Guimaraes <lucasgfortes24@gmail.com>
2023-02-28 00:04:13 -0300

    subindo model do professor

--- server/src/models/professor.model.js
+++ server/src/models/professor.model.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+
+const professorSchema = new mongoose.Schema({
+  nome: String,
+  senha: String,
+  email: String,
+  user: String,
+});
+
+module.exports = professorSchema;
