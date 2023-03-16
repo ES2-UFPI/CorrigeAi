f34b648f8e4e2570df79a22b76263ce7587c9e88
jaumVitor <joao.vitor.pires.060@gmail.com>
2023-03-16 09:21:00 -0300

    Delete question, without fixing numberQuestions #38

--- vite-client/src/styles.ts
+++ vite-client/src/styles.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 
-export const AppStyles = styled.div`
-  background: var(--twitter);
-  width: 100%; 
-  height: 100%;
-  padding: 5rem 5rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
+// export const AppStyles = styled.div`
+//   background: var(--twitter);
+//   width: 100%; 
+//   height: 100%;
+//   padding: 5rem 5rem;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `
