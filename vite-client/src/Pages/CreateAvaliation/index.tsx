95e9a9ee834e743fdc515d902f9d2d8e507fccf0
jaumVitor <joao.vitor.pires.060@gmail.com>
2023-03-20 00:23:30 -0300

    Fix layout wrapper in pages

--- vite-client/src/Pages/CreateAvaliation/index.tsx
+++ vite-client/src/Pages/CreateAvaliation/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 //Components da aplicação
 import { FormAvaliation } from '../../components/FormAvaliation'
 import { ContextQuestionsProvider } from '../../context/contextQuestions'
+import { Wrapper } from '../../styles/Layout'
 
 //Components de estilização
 import { CreateTaskOrExamStyled } from './styles'
@@ -19,28 +20,30 @@ export function CreateTaskOrExam() {
   return (
     <ContextQuestionsProvider>
       <CreateTaskOrExamStyled className="TaskOrExam">
-        <label htmlFor="avaliation">Escolha a avaliação: </label>
-        <select
-          name="avaliation"
-          id="avaliation"
-          value={selectValue}
-          onChange={e => setSelectValue(e.target.value)}
-        >
-          {list.map((avaliation, i) => (
-            <option key={i} value={avaliation.id}>
-              {avaliation.name}
-            </option>
-          ))}
-        </select>
-        {/* Dependendo da escolha, vai gerar um formulario diferente */}
-        { 
-          selectValue !== 'none' ? 
-            <FormAvaliation 
-              typeAvaliation={selectValue}
-            /> 
-            : 
-            <h2>Escolha uma opção</h2>
-        }
+        <Wrapper>
+          <label htmlFor="avaliation">Escolha a avaliação: </label>
+          <select
+            name="avaliation"
+            id="avaliation"
+            value={selectValue}
+            onChange={e => setSelectValue(e.target.value)}
+          >
+            {list.map((avaliation, i) => (
+              <option key={i} value={avaliation.id}>
+                {avaliation.name}
+              </option>
+            ))}
+          </select>
+          {/* Dependendo da escolha, vai gerar um formulario diferente */}
+          {
+            selectValue !== 'none' ?
+              <FormAvaliation
+                typeAvaliation={selectValue}
+              />
+              :
+              <h2>Escolha uma opção</h2>
+          }
+        </Wrapper>
       </CreateTaskOrExamStyled>
     </ContextQuestionsProvider>
   )
--- vite-client/src/styles/Layout.ts
+++ vite-client/src/styles/Layout.ts
@@ -4,9 +4,11 @@ export const Wrapper = styled.div `
   display: flex;
   flex-direction: column;
   align-items: center;
+
   width: 100%;
   height: 100%;
   max-width: 800px;
+  padding: 0 2rem;
   margin: 0 auto;  
 `;
 
